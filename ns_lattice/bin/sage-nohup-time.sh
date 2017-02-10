b2f6bc5aba344f7762ed4163a33d4e3a284f22fc
Niels Lubbes <niels.lubbes@noadds>
2017-02-10 23:09:35 +0100

    Modified script for remote execution.

--- ns_lattice/bin/sage-nohup-time.sh
+++ ns_lattice/bin/sage-nohup-time.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
 export PATH=$PATH:.
-export PYTHONPATH=$PYTHONPATH:../src/
+export PYTHONPATH=$PYTHONPATH:.
 export OUTPUT_PATH=/home/LOCAL/nlubbes/OUTPUT/
 
-nohup time nice /home/software/sage-7.3/sage -python ../src/ns_lattice/__main__.py  > out 2> err < /dev/null &
+rm err out
+nohup time nice /home/software/sage-7.3/sage -python __main__.py  > out 2> err < /dev/null &
+cat err
+cat out
 
